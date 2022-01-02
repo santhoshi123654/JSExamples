9184f45fc2e80d381f0d984ef4a6630dbcad692c
“santhoshi123654” <santhoshi.badam@gmail.com>
2022-01-02 21:30:47 +0530

    Added:min avg random Numbers Program

---
+++ minAvgRandom.js
@@ -0,0 +1,16 @@
+let a=Math.floor(Math.random()*90)+10;
+let b=Math.floor(Math.random()*90)+10;
+let c=Math.floor(Math.random()*90)+10;
+let d=Math.floor(Math.random()*90)+10;
+let e=Math.floor(Math.random()*90)+10;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+let sum = a+b+c+d+e;
+let avg = (a+b+c+d+e)/5;
+
+console.log("The Sum of Five Random Numbers are :" +sum);
+console.log("The Average of random numbers are :" + avg);
