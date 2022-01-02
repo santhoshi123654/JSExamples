203b39512764586162f9c766c5d7804f3b62035c
“santhoshi123654” <santhoshi.badam@gmail.com>
2022-01-02 21:29:30 +0530

    Added:Random Numbers Program

---
+++ addRandomNums.js
@@ -0,0 +1,5 @@
+let x = Math.floor(Math.random()*6)+1;
+let y = Math.floor(Math.random()*6)+1;
+console.log("The Dice One is :" +x);
+console.log("The  Dice Two is :" +y);
+console.log("Sum of Random Number of Dice is:"+ (x+y));
